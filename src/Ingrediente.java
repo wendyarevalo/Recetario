6ca23a919ba5821f848aa0119e32194775b8632f
Wendy Yunuen Arévalo Espinal <wendy@pickupon.io>
2022-03-10 11:37:49 -0600

    Clase Ingrediente completa

--- src/Ingrediente.java
+++ src/Ingrediente.java
@@ -1,2 +1,37 @@
 public class Ingrediente {
+    private String nombre;
+    private boolean esSalado;
+    private double peso;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isEsSalado() {
+        return esSalado;
+    }
+
+    public void setEsSalado(boolean esSalado) {
+        this.esSalado = esSalado;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Ingrediente(String nombre, boolean esSalado, double peso) {
+        this.nombre = nombre;
+        this.esSalado = esSalado;
+        this.peso = peso;
+        System.out.println("Ingrediente creado");
+    }
+
 }

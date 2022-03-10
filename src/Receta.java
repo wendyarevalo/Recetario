b5f37cf65ee1df9cc8e18466350969e83036c96a
Wendy Yunuen Arévalo Espinal <wendy@pickupon.io>
2022-03-10 11:48:44 -0600

    Clase Receta completa

---
+++ src/Receta.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Receta {
+    private double tiempoPreparacion;
+    private ArrayList<Ingrediente> ingredientes;
+    private ArrayList<String> pasos;
+
+    public Receta(double tiempoPreparacion, ArrayList<Ingrediente> ingredientes, ArrayList<String> pasos) {
+        this.tiempoPreparacion = tiempoPreparacion;
+        this.ingredientes = ingredientes;
+        this.pasos = pasos;
+        System.out.println("Receta creada");
+    }
+
+    public double getTiempoPreparacion() {
+        return tiempoPreparacion;
+    }
+
+    public void setTiempoPreparacion(double tiempoPreparacion) {
+        this.tiempoPreparacion = tiempoPreparacion;
+    }
+
+    public ArrayList<Ingrediente> getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(ArrayList<Ingrediente> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public ArrayList<String> getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(ArrayList<String> pasos) {
+        this.pasos = pasos;
+    }
+}
